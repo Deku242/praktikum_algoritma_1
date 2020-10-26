051a9f5f38edea5ffec41c25ddbf5b82484f93bd
Deku242 <ilham.hernowo10@gmail.com>
2020-10-26 20:43:15 +0700

    pengumpulan tugas minggu 2

---
+++ minggu-2/asset/F_123200168_Modul2no1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+cout<<" Nama	: Muhammad Ilham Nursastrawan\n";
+cout<<" \n Nim	: 123200168 \n";
+cout<<" \n Kelas	: F \n";
+cout<<" \n Jenis Kelamin	: Laki-Laki \n";
+cout<<" \n Alamat	: Perum Selo Permata Asri No. 30 \n";
+cout<<" \n E-mail	: ilham.hernowo10@gmail.com \n";
+cout<<" \n No. Hp	: 08895805150";
+  
+}
+	
---
+++ minggu-2/asset/F_123200168_Modul3no1.cpp
@@ -0,0 +1,29 @@
+ #include <iostream>
+using namespace std;
+ #define PHI 3.14
+ int main ()
+ {
+	float r,s;
+	float Kerucut,selimut;
+	int phi=3.14;
+	
+	cout << " Program Pengerjaan Luas Dan Selimut Kerucut\n";
+	
+	cout << " \n Muhammad Ilham Nursastrawan_123200168\n";
+	
+	cout << " \n masukkan Jari Jari : ";
+	cin >> r;
+	
+	cout << " masukkan garis pelukis : ";
+	cin >> s; 
+	
+	
+	Kerucut   = phi*r*(r+s);
+	selimut   = phi*r*s;
+	
+	
+	cout <<" Luas permukaan Kerucut adalah		: "<<Kerucut<<endl;
+	cout <<" Luas permukaan selimut adalah		: "<<selimut<<endl;
+	return(0);
+		
+ }
